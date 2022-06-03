540ab3b46f5e90649cb5fb12c83c053a957e4712
Michael Clerx <michael.clerx@nottingham.ac.uk>
2022-06-03 18:27:52 +0100

    Tweaks

--- f4-correlation.py
+++ f4-correlation.py
@@ -24,6 +24,111 @@ with base.connect() as con:
     a = [row for row in c.execute(q)]
 
 
+
+def gather(query, filename1, filename2):
+    """
+    Queries the DB for reports of both activation and inactivation and does
+    some analysis:
+
+    1. Writes a CSV file containing the midpoints plus 2 sigma intervals.
+    2. Fits a linear regression line.
+    3. Writes a 2nd CSV file containing the midpoints minus the regression.
+    row.
+    4. Creates and returns a table row.
+
+    The returned row contains the fields ``(nreports, a, b, p, sd_diff)``,
+    where ``nreports`` is the number of reports used, ``a`` and ``b`` are the
+    regression offset and slope, ``p`` is the Pearson correlation coefficient,
+    and ``sd_diff`` is the standard deviation of ``V_a - V_i``.
+    """
+    # Query db
+    data = []
+    with base.connect() as con:
+        c = con.cursor()
+        for row in c.execute(query):
+            data.append((
+                row['pub'],
+                row['va'],
+                row['stda'] * 2,
+                row['vi'],
+                row['stdi'] * 2,
+                row['va'] - row['vi'],
+            ))
+
+    # Write first file
+    path = os.path.join(base.DIR_DATA_OUT, filename1)
+    print(f'Writing to {os.path.relpath(path)}')
+    with open(path, 'w') as f:
+        csv = base.csv_writer(f)
+        csv.writerow(['pub', 'va', '+-', 'vi', '+-', 'dv'])
+        for row in data:
+            csv.writerow(row)
+
+    #
+    # Correct with linear regression
+    #
+    # Gather data
+    va = []
+    vi = []
+    for row in data:
+        va.append(row[1])
+        vi.append(row[3])
+    va = np.array(va)
+    vi = np.array(vi)
+
+    # Fit line
+    b, a = np.polyfit(va, vi, 1)
+
+    # Subtract and write to file
+    path = os.path.join(base.DIR_DATA_OUT, filename2)
+    print(f'Writing to {os.path.relpath(path)}')
+    with open(path, 'w') as f:
+        csv = base.csv_writer(f)
+        csv.writerow(['pub', 'va', '+-', 'vic', '+-'])
+        for k, row in enumerate(data):
+            row = list(row[:-1])
+            row[3] = row[3] - (a + b * row[1])
+            csv.writerow(row)
+
+    # Get Pearson correlation coefficient
+    p = np.corrcoef(va, vi)[1, 0]
+
+    # Get standard deviation of Va - Vi
+    sd_diff = np.std(va - vi)
+
+    # Return row
+    return [len(data), a, b, p, sd_diff]
+
+
+
+
+'''
+    head = ['', 'reports', 'a', 'b', 'Pearson', 'Sigma Va-Vi']
+    rows = []
+
+    # All data
+    q = 'select pub, va, stda, vi, stdi from midpoints_wt'
+    q += ' where va != 0'
+    q += ' AND vi != 0'
+    q += ' AND stda != 0'
+    q += ' AND stdi != 0'
+    fname1 = 'midpoint-correlations.csv'
+    fname2 = 'midpoint-correlations-regression.csv'
+    rows.append(['All'] + gather(q, fname1, fname2))
+
+    # Largest subgroup
+    q += ' and sequence == "astar"'
+    q += ' and cell == "HEK"'
+    q += ' and beta1 == "yes"'
+    fname1 = 'midpoint-correlations-largest-subgroup.csv'
+    fname2 = 'midpoint-correlations-regression-largest-subgroup.csv'
+    rows.append(['Most common'] + gather(q, fname1, fname2))
+
+    print()
+    base.table(head, rows)
+'''
+
+
 #
 # Create figure
 #
@@ -32,63 +137,6 @@ fig = plt.figure(figsize=(6, 6))    # Two-third column size
 fig.subplots_adjust(0.04, 0.075, 0.99, 0.99)
 
 ax = fig.add_subplot(1, 1, 1)
-ax.set_xlabel('Membrane potential (mV)')
-ax.set_xlim(-120, 0)
-ax.set_ylim(-2, 187)
-for s in ax.spines.values():
-    s.set_visible(False)
-ax.spines['bottom'].set_visible(True)
-ax.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(20))
-ax.xaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(5))
-ax.get_yaxis().set_visible(False)
-
-sstd = dict(color='#999999')
-ssem = dict(color='k', lw=3)
-ca = 'tab:blue'
-ci = 'tab:orange'
-m = 'o'
-ms = 3
-
-offset = max(0, (len(a) - len(i)) / 2)
-for k, d in enumerate(i):
-    mu, sem, std, seq, bet, cell = d
-    st = dict(color=ci, markersize=ms, zorder=3)
-    if seq == 'astar' and bet == 'yes' and cell == 'HEK':
-        st = dict(markeredgecolor='k', markerfacecolor='w', markersize=ms + 2,
-                  zorder=4)
-
-    k += offset
-    ax.plot((mu - 2 * std, mu + 2 * std), (k, k), **sstd, zorder=1)
-    ax.plot((mu - sem, mu + sem), (k, k), **ssem, zorder=2)
-    ax.plot(mu, k, m, **st)
-
-offset = max(0, (len(i) - len(a)) / 2)
-for k, d in enumerate(a):
-    mu, sem, std, seq, bet, cel = d
-    st = dict(color=ca, markersize=ms, zorder=3)
-    if seq == 'astar' and bet == 'yes' and cell == 'HEK':
-        st = dict(markeredgecolor='k', markerfacecolor='w', markersize=ms + 2,
-                  zorder=4)
-
-    k += offset
-    ax.plot((mu - 2 * std, mu + 2 * std), (k, k), **sstd, zorder=1)
-    ax.plot((mu - sem, mu + sem), (k, k), **ssem, zorder=2)
-    ax.plot(mu, k, m, **st)
-
-ms2 = 12
-elements = [
-    matplotlib.lines.Line2D([0], [0], marker=m, color='w', label='$V_a$',
-                            markersize=ms2, markerfacecolor=ca),
-    matplotlib.lines.Line2D([0], [0], marker=m, color='w', label='$V_i$',
-                            markersize=ms2, markerfacecolor=ci),
-    matplotlib.lines.Line2D([0], [0], marker=m, color='w', markersize=ms2,
-                            markerfacecolor='w', markeredgecolor='k',
-                            label=r'a*, ${\beta}1$, HEK',),
-    matplotlib.lines.Line2D([0], [0], color=ssem['color'], label='SEM',
-                            lw=ssem['lw']),
-    matplotlib.lines.Line2D([0], [0], color=sstd['color'], label='STD'),
-]
-ax.legend(loc=(-0.04, 0.80), frameon=False, handles=elements)
 
 
 fname = 'f4-correlation.pdf'
