0b7afba24ae321aa61f85ba9753d5b137b5c73aa
Michael Clerx <michael.clerx@nottingham.ac.uk>
2024-03-28 15:21:55 +0000

    Tidying up

--- d5-temperatures.py
+++ d5-temperatures.py
@@ -4,18 +4,21 @@
 #
 import base
 
+nooocytes = True
+qo = 'where cell != "Oocyte"' if nooocytes else ''
 
 with base.connect() as con:
     c = con.cursor()
 
     # Get lowest and highest T reported
-    row = next(c.execute('select min(tmin), max(tmax) from midpoints_wt'))
+    row = next(c.execute(
+        f'select min(tmin), max(tmax) from midpoints_wt {qo}'))
     print(f'Minimum T: {row[0]}')
     print(f'Maximum T: {row[1]}')
 
     # Get smallest, biggest, mean delta-T reported
     q = ('select min(tmax-tmin), max(tmax-tmin), avg(tmax-tmin)'
-         ' from midpoints_wt')
+         f' from midpoints_wt {qo}')
     row = next(c.execute(q))
     print(f'Minimum dT: {row[0]}')
     print(f'Maximum dT: {row[1]}')
